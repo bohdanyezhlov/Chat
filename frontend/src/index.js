2cd3fab6f431be3e9743c852adde7e6c12c82c91
Bohdan2241 <bohdanyezhlov@gmail.com>
2023-03-28 22:01:05 +0200

    fix linter

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Routes from './Routes';
+import axios from 'axios';
 import './index.css';
 
 const rootElement = document.getElementById('root');
-
+axios
+  .post('/api/v1/login', { username: 'admin', password: 'admin' })
+  .then((response) => {
+    console.log(response.data); // => { token: ..., username: 'admin' }
+  });
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <Routes />
