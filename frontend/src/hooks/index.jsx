f45cbb6f0188b725f9cf61d53e991d5d85fbb00e
Bohdan2241 <bohdanyezhlov@gmail.com>
2023-04-13 17:52:29 +0200

    change extension from jsx to js

--- frontend/src/hooks/index.jsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-
-import { AuthContext, SocketContext } from '../contexts';
-
-export const useAuth = () => useContext(AuthContext);
-
-export const useSocket = () => useContext(SocketContext);
