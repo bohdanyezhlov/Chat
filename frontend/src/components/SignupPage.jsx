a28fb8da92dc422c599d1fbea51cf7adb4ed119a
Bohdan2241 <bohdanyezhlov@gmail.com>
2023-04-16 21:59:18 +0200

    fix html

--- frontend/src/components/SignupPage.jsx
+++ frontend/src/components/SignupPage.jsx
@@ -71,108 +71,111 @@ const Signup = () => {
   });
 
   return (
-    <div className="row justify-content-center align-content-center h-100">
-      <div className="col-12 col-md-8 col-xxl-6">
-        <div className="card shadow-sm">
-          <div className="card-body d-flex flex-column flex-md-row justify-content-around align-items-center p-5">
-            <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
-              <img
-                className="rounded-circle"
-                alt={t('signup.header')}
-                src={signupImage}
-              />
-            </div>
-            <Form
-              onSubmit={formik.handleSubmit}
-              className="col-12 col-md-6 mt-3 mt-mb-0"
-            >
-              <h1 className="text-center mb-4">{t('signup.header')}</h1>
-              <Form.Group
-                controlId="formBasicUsername"
-                className="form-floating mb-3"
+    <div className="container-fluid h-100">
+      <div className="row justify-content-center align-content-center h-100">
+
+        <div className="col-12 col-md-8 col-xxl-6">
+          <div className="card shadow-sm">
+            <div className="card-body d-flex flex-column flex-md-row justify-content-around align-items-center p-5">
+              <div>
+                <img
+                  className="rounded-circle"
+                  alt={t('signup.header')}
+                  src={signupImage}
+                />
+              </div>
+              <Form
+                onSubmit={formik.handleSubmit}
+                className="w-50"
               >
-                <Form.Control
-                  type="text"
-                  name="username"
-                  placeholder={t('signup.usernameConstraints')}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.username}
-                  ref={inputRef}
-                  required
-                  isInvalid={
+                <h1 className="text-center mb-4">{t('signup.header')}</h1>
+                <Form.Group
+                  controlId="formBasicUsername"
+                  className="form-floating mb-3"
+                >
+                  <Form.Control
+                    type="text"
+                    name="username"
+                    placeholder={t('signup.usernameConstraints')}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.username}
+                    ref={inputRef}
+                    required
+                    isInvalid={
                     signupFailed
                     || (formik.touched.username && formik.errors.username)
                   }
-                />
-                <Form.Label>{t('signup.username')}</Form.Label>
-                {formik.touched.username && formik.errors.username && (
+                  />
+                  <Form.Label>{t('signup.username')}</Form.Label>
+                  {formik.touched.username && formik.errors.username && (
                   <div className="invalid-tooltip">
                     {t(formik.errors.username)}
                   </div>
-                )}
-              </Form.Group>
+                  )}
+                </Form.Group>
 
-              <Form.Group
-                controlId="formBasicPassword"
-                className="form-floating mb-4"
-              >
-                <Form.Control
-                  type="password"
-                  name="password"
-                  placeholder={t('signup.passMin')}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.password}
-                  required
-                  isInvalid={
+                <Form.Group
+                  controlId="formBasicPassword"
+                  className="form-floating mb-3"
+                >
+                  <Form.Control
+                    type="password"
+                    name="password"
+                    placeholder={t('signup.passMin')}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.password}
+                    required
+                    isInvalid={
                     signupFailed
                     || (formik.touched.password && formik.errors.password)
                   }
-                />
-                <Form.Label>{t('signup.password')}</Form.Label>
-                {formik.touched.password && formik.errors.password && (
+                  />
+                  <Form.Label>{t('signup.password')}</Form.Label>
+                  {formik.touched.password && formik.errors.password && (
                   <div className="invalid-tooltip">
                     {t(formik.errors.password)}
                   </div>
-                )}
-              </Form.Group>
+                  )}
+                </Form.Group>
 
-              <Form.Group
-                controlId="formConfirmPassword"
-                className="form-floating mb-4"
-              >
-                <Form.Control
-                  type="password"
-                  name="confirmPassword"
-                  placeholder={t('signup.mustMatch')}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.confirmPassword}
-                  required
-                  isInvalid={
+                <Form.Group
+                  controlId="formConfirmPassword"
+                  className="form-floating mb-4"
+                >
+                  <Form.Control
+                    type="password"
+                    name="confirmPassword"
+                    placeholder={t('signup.mustMatch')}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.confirmPassword}
+                    required
+                    isInvalid={
                     signupFailed
                     || (formik.touched.confirmPassword
                       && formik.errors.confirmPassword)
                   }
-                />
-                <Form.Label>{t('signup.confirm')}</Form.Label>
-                {formik.touched.confirmPassword
+                  />
+                  <Form.Label>{t('signup.confirm')}</Form.Label>
+                  {formik.touched.confirmPassword
                   && formik.errors.confirmPassword && (
                     <div className="invalid-tooltip">
                       {t(formik.errors.confirmPassword)}
                     </div>
-                )}
-              </Form.Group>
+                  )}
+                </Form.Group>
 
-              <Button
-                type="submit"
-                disabled={formik.isSubmitting}
-                className="w-100 mb-3"
-              >
-                {t('signup.submit')}
-              </Button>
-            </Form>
+                <Button
+                  type="submit"
+                  disabled={formik.isSubmitting}
+                  className="w-100"
+                >
+                  {t('signup.submit')}
+                </Button>
+              </Form>
+            </div>
           </div>
         </div>
       </div>
